602df253e8c451a2431d279b436ce3a08c2cdf6b
Samuel Amoah <snamoah@gmail.com>
2016-01-10 01:06:22 +0000

    bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'snamoah:useragent',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: 'Meteor package for detecting platform details',
   // URL to the Git repository containing the source code for this package.
